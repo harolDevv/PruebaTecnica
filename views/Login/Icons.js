f4985551f879497d90f5e7604029c34f4a952995
Harold Tenorio Galvan <harold.tenorio@epn.edu.ec>
2024-03-25 04:05:05 -0500

    first commit

---
+++ views/Login/Icons.js
@@ -0,0 +1,86 @@
+export const UserIcon = () => {
+    return(
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+    )
+}
+
+export const PasswordIcon = () => {
+    return(
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-key"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0z" /><path d="M15 9h.01" /></svg>
+    )
+}
+
+export const LoginIcon = () => {
+    return(
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-login"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M21 12h-13l3 -3" /><path d="M11 15l-3 -3" /></svg>
+    )
+}
+
+export const CreateAccount = () => {
+    return(
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" /><path d="M12 9v6" /></svg> 
+    )
+}
+
+export const EyeFilledIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="1em"
+    {...props}
+  >
+    <path
+      d="M21.25 9.14969C18.94 5.51969 15.56 3.42969 12 3.42969C10.22 3.42969 8.49 3.94969 6.91 4.91969C5.33 5.89969 3.91 7.32969 2.75 9.14969C1.75 10.7197 1.75 13.2697 2.75 14.8397C5.06 18.4797 8.44 20.5597 12 20.5597C13.78 20.5597 15.51 20.0397 17.09 19.0697C18.67 18.0897 20.09 16.6597 21.25 14.8397C22.25 13.2797 22.25 10.7197 21.25 9.14969ZM12 16.0397C9.76 16.0397 7.96 14.2297 7.96 11.9997C7.96 9.76969 9.76 7.95969 12 7.95969C14.24 7.95969 16.04 9.76969 16.04 11.9997C16.04 14.2297 14.24 16.0397 12 16.0397Z"
+      fill="currentColor"
+    />
+    <path
+      d="M11.9984 9.14062C10.4284 9.14062 9.14844 10.4206 9.14844 12.0006C9.14844 13.5706 10.4284 14.8506 11.9984 14.8506C13.5684 14.8506 14.8584 13.5706 14.8584 12.0006C14.8584 10.4306 13.5684 9.14062 11.9984 9.14062Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+
+export const EyeSlashFilledIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="1em"
+    {...props}
+  >
+    <path
+      d="M21.2714 9.17834C20.9814 8.71834 20.6714 8.28834 20.3514 7.88834C19.9814 7.41834 19.2814 7.37834 18.8614 7.79834L15.8614 10.7983C16.0814 11.4583 16.1214 12.2183 15.9214 13.0083C15.5714 14.4183 14.4314 15.5583 13.0214 15.9083C12.2314 16.1083 11.4714 16.0683 10.8114 15.8483C10.8114 15.8483 9.38141 17.2783 8.35141 18.3083C7.85141 18.8083 8.01141 19.6883 8.68141 19.9483C9.75141 20.3583 10.8614 20.5683 12.0014 20.5683C13.7814 20.5683 15.5114 20.0483 17.0914 19.0783C18.7014 18.0783 20.1514 16.6083 21.3214 14.7383C22.2714 13.2283 22.2214 10.6883 21.2714 9.17834Z"
+      fill="currentColor"
+    />
+    <path
+      d="M14.0206 9.98062L9.98062 14.0206C9.47062 13.5006 9.14062 12.7806 9.14062 12.0006C9.14062 10.4306 10.4206 9.14062 12.0006 9.14062C12.7806 9.14062 13.5006 9.47062 14.0206 9.98062Z"
+      fill="currentColor"
+    />
+    <path
+      d="M18.25 5.74969L14.86 9.13969C14.13 8.39969 13.12 7.95969 12 7.95969C9.76 7.95969 7.96 9.76969 7.96 11.9997C7.96 13.1197 8.41 14.1297 9.14 14.8597L5.76 18.2497H5.75C4.64 17.3497 3.62 16.1997 2.75 14.8397C1.75 13.2697 1.75 10.7197 2.75 9.14969C3.91 7.32969 5.33 5.89969 6.91 4.91969C8.49 3.95969 10.22 3.42969 12 3.42969C14.23 3.42969 16.39 4.24969 18.25 5.74969Z"
+      fill="currentColor"
+    />
+    <path
+      d="M14.8581 11.9981C14.8581 13.5681 13.5781 14.8581 11.9981 14.8581C11.9381 14.8581 11.8881 14.8581 11.8281 14.8381L14.8381 11.8281C14.8581 11.8881 14.8581 11.9381 14.8581 11.9981Z"
+      fill="currentColor"
+    />
+    <path
+      d="M21.7689 2.22891C21.4689 1.92891 20.9789 1.92891 20.6789 2.22891L2.22891 20.6889C1.92891 20.9889 1.92891 21.4789 2.22891 21.7789C2.37891 21.9189 2.56891 21.9989 2.76891 21.9989C2.96891 21.9989 3.15891 21.9189 3.30891 21.7689L21.7689 3.30891C22.0789 3.00891 22.0789 2.52891 21.7689 2.22891Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const AdressIcon = () => {
+    return(
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-address-book"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6v12a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2z" /><path d="M10 16h6" /><path d="M13 11m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M4 8h3" /><path d="M4 12h3" /><path d="M4 16h3" /></svg>
+    )
+}
---
+++ views/Login/Login.jsx
@@ -0,0 +1,98 @@
+'use client'
+
+//next ui imports
+import {Input, Button} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+//react imports
+import { useState } from "react";
+
+//icons
+import { EyeFilledIcon, EyeSlashFilledIcon, LoginIcon, PasswordIcon, UserIcon, CreateAccount, AdressIcon} from "./Icons";
+
+
+export const Login = () => {
+
+    const router = useRouter()
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const [inState, setInState] = useState(
+        {
+            user:'',
+            password:''
+        }
+    )
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        if(inState.user == '123' && inState.password == '123'){
+            router.push('/Home')
+            router.refresh()
+        }
+    }
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="my-3">
+            <h1 style={{fontSize:'35px', margin:'20px 0px', display:'flex', alignItems:'center'}}><AdressIcon/> Dummy<em style={{color:''}}>Dress</em></h1>
+        </div>
+        <form style={{width:'35%', minWidth:'340px'}} onSubmit={(e) => handleLogin(e)}>
+            <Input
+            onChange={(e) => setInState({...inState, [e.target.name]: e.target.value})}
+            name="user"
+                isRequired
+                size='lg'
+                type="tel"
+                label="Usuario"
+                placeholder="Numero de cedula"
+                labelPlacement='inside'
+                variant="bordered"
+                startContent={
+                <UserIcon className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+            />
+            <Input
+            onChange={(e) => setInState({...inState, [e.target.name]: e.target.value})}
+            name="password"
+            isRequired
+                size="lg"
+                className="mt-2"
+                type={isVisible ? "text" : "password"}
+                label="Contraseña"
+                variant="bordered"
+                placeholder="Contraseña"
+                labelPlacement='inside'
+                endContent={
+                    <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                    {isVisible ? (
+                        <EyeSlashFilledIcon className="text-xl text-default-400 pointer-events-none" />
+                    ) : (
+                        <EyeFilledIcon className="text-xl text-default-400 pointer-events-none" />
+                    )}
+                    </button>
+                }
+                startContent={
+                <PasswordIcon  className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+                
+            />
+            <Button type="submit" className="w-full mt-2" color="success" variant="ghost" startContent={<LoginIcon/>} size='lg'>
+                Iniciar sesion
+            </Button>
+            
+            <div className="flex items-center">
+                <div className="text-center h-1"></div>
+                <span className="flex-1 text-center">o</span>
+                <div className="text-center h-1"></div>
+            </div>
+
+            <Button className="w-full mt-2" color="" variant="ghost" startContent={<CreateAccount/>} size='lg'>
+                Crear cuenta
+            </Button>
+        </form>
+    </div>
+  )
+}
